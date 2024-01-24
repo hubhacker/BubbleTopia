8251c86435fb841ed911bdfb744a7514500f2c5d
Vivian Oh <116562546+voh919@users.noreply.github.com>
2024-01-23 19:49:00 -0500

    rcw

--- textbox.gd
+++ textbox.gd
@@ -20,5 +20,6 @@ func add_text(next_text):
 	label.text = next_text
 	show_textbox()
 	var tween = create_tween()
-	tween.interpolate_value(label, "percent_visible", 0.0, 1.0, len(next_text) * CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.tween_property(label, "percent_visible", 0.0, 1.0).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
+	tween.len(next_text) * CHAR_READ_RATE
 	tween.start()
