ac8219295675c69098343feca48ad2ce5890afce
Vivian Oh <116562546+voh919@users.noreply.github.com>
2024-01-29 23:13:53 -0500

    facc

--- textbox.gd
+++ textbox.gd
@@ -20,9 +20,11 @@ func add_text(next_text):
 	label.text = next_text
 	show_textbox()
 	
-	var tween = create_tween()
-	var time = CHAR_READ_RATE
-	tween.interpolate_value(label, "visible_characters", 0.0, 1.0, len(next_text)*CHAR_READ_RATE, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	var tween = get_tree().create_tween()
+	#tween.interpolate_value(label, "visible_characters", 0.0, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
 	#tween.tween_property(label, "visible_characters", 0.0, 1.0).set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_OUT)
-	len(next_text) * CHAR_READ_RATE
+	#len(next_text) * CHAR_READ_RATE
+	#tween.tween_property(label, "display_text", 0.0,1.0, len(next_text) * CHAR_READ_RATE,Tween.TRANS_LINEAR,Tween.EASE_OUT)
+	tween.tween_property(label, "visible_ratio", 1.0, len(next_text)*CHAR_READ_RATE)
+	tween.connect("finished", Tween.on_tween_finished)
 	tween.play()
