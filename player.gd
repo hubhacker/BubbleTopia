0e427ae873ab4a2f0774873630ba96fd9c7644bb
sarahspe <116048109+sarahspe@users.noreply.github.com>
2024-01-30 22:02:18 -0500

    dialogue

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const max_speed = 400
+const max_speed = 600
 const accel = 1500
 const friction = 600
 
@@ -11,14 +11,6 @@ var input = Vector2.ZERO
 func _ready():
 	anim.play("idle")
 
-func _input(event: InputEvent):
-	# check if a dialog is already running
-	if Dialogic.current_timeline != null:
-		return
-
-	if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
-		Dialogic.start('chapterA')
-		get_viewport().set_input_as_handled()
 
 func _physics_process(delta):
 	player_movement(delta)
