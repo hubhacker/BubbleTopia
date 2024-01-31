279f0c7f28d7b2c1401183ee9e083d53e2d3efea
jolene <jolliu26@bergen.org>
2024-01-30 21:51:46 -0500

    rewriting
    d

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -1,29 +1,11 @@
 extends CharacterBody2D
 
-const max_speed = 400
-const accel = 1500
-const friction = 600
+@export var speed : int = 65
 
-var input = Vector2.ZERO
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection*speed
 
 func _physics_process(delta):
-	player_movement(delta)
-	
-func get_input():
-	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
-	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	return input.normalized()
-	
-func player_movement(delta):
-	input = get_input()
-	
-	if input == Vector2.ZERO:
-		if velocity.length() > (friction * delta):
-			velocity -= velocity.normalized() * (friction * delta)
-		else:
-			velocity = Vector2.ZERO
-	else:
-		velocity += (input * accel * delta)
-		velocity = velocity.limit_length(max_speed)
-		
+	handleInput()
 	move_and_slide()
