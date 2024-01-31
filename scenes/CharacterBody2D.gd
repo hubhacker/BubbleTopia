e00dfd9b29d74cfe08d8eb763301f6b32fcffbaa
jolene <jolliu26@bergen.org>
2024-01-31 12:11:57 -0500

    dialog
    sss

--- scenes/CharacterBody2D.gd
+++ scenes/CharacterBody2D.gd
@@ -10,6 +10,10 @@ func _ready():
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		DialogueManager.show_example_dialogue_balloon(load("res://Dialogue/main.dialogue"), "scene1")
+		return
 
 func _physics_process(delta):
 	handleInput()
