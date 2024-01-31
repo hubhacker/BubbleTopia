8ab8f2333cc806f6c3e83da5c70da1fab3595f41
jolene <jolliu26@bergen.org>
2024-01-30 22:34:01 -0500

    PUSHIn

--- player.gd
+++ player.gd
@@ -1,18 +1,11 @@
 extends CharacterBody2D
 
-const max_speed = 600
+const max_speed = 400
 const accel = 1500
 const friction = 600
 
-@onready var anim = $AnimatedSprite2D
-
 var input = Vector2.ZERO
 
-<<<<<<< HEAD
-func _ready():
-	anim.play("idle")
-
-=======
 #func _input(event: InputEvent):
 	## check if a dialog is already running
 	#if Dialogic.current_timeline != null:
@@ -21,7 +14,6 @@ func _ready():
 	#if event is InputEventKey and event.keycode == KEY_ENTER and event.pressed:
 		#Dialogic.start('chapterA')
 		#get_viewport().set_input_as_handled()
->>>>>>> 279f0c7f28d7b2c1401183ee9e083d53e2d3efea
 
 func _physics_process(delta):
 	player_movement(delta)
